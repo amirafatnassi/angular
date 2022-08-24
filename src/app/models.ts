53eb85d864ebdf9054452307f137c830f605be3d
unknown <amirafatnassi.dev@gmail.com>
2022-08-24 20:58:49 +0200

    update env

---
+++ src/app/models.ts
@@ -0,0 +1,47 @@
+export interface Game {
+    background_imageg: string;
+    name: string;
+    released: string;
+    metacritic_url: string;
+    website: string;
+    description: string;
+    metacritic: number;
+    genres: Array<Genre>;
+    parent_platforms: Array<ParentPlatform>;
+    publishers: Array<Publishers>;
+    ratings: Array<Rating>;
+    screenshorts: Array<Screeshots>;
+    trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+    results: Array<T>;
+}
+
+interface Genre {
+    name: string;
+}
+
+interface ParentPlatform {
+    platform: {
+        name: string;
+    };
+}
+
+interface Publishers {
+    name: string;
+}
+
+interface Rating {
+    id: number;
+    count: number;
+    title: string;
+}
+
+interface Screeshots {
+    image: string;
+}
+
+interface Trailer {
+    data: { max: string; };
+}
