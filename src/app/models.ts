7a71c8c0cc21d4ad5b62e45cf4d382ad770e5f35
amirafatnassi <amirafatnassi88@gmail.com>
2022-08-25 22:50:41 +0100

    implementing data into tabs

--- src/app/models.ts
+++ src/app/models.ts
@@ -10,7 +10,7 @@ export interface Game {
     parent_platforms: Array<ParentPlatform>;
     publishers: Array<Publishers>;
     ratings: Array<Rating>;
-    screenshorts: Array<Screeshots>;
+    screenshots: Array<Screeshots>;
     trailers: Array<Trailer>;
 }
 
